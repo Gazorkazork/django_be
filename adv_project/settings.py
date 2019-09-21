8a8fcb488936822540be4b7c32b3a58d4e87972c
BrannanC <brannanconrad@gmail.com>
2019-09-21 15:24:48 -0500

    Uses postgres on heroku

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -97,10 +97,7 @@ WSGI_APPLICATION = 'adv_project.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default=config('DATABASE_URL'), conn_max_age=600)
 }
 
 # Password validation
