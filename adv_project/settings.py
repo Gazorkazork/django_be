d61c01d428f559f05a98c01bac0cc91d0a9a109b
BrannanC <brannanconrad@gmail.com>
2019-09-23 18:35:54 -0500

    Trying stuff

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS')
+ALLOWED_HOSTS = [".herokuapp.com",  "127.0.0.1",
+                 "gazorkazork.netlify.com", "localhost"]
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
