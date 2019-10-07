4300bc938c42a826c7e143c2bb91713b4a44d1b5
Matt Klein <mklein246@gmail.com>
2019-10-07 15:28:25 -0700

    started work on mob models

--- adventure/models.py
+++ adventure/models.py
@@ -1,9 +1,9 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
-import uuid
 
 class Room(models.Model):
     title = models.CharField(max_length=50, default="DEFAULT TITLE")
@@ -36,13 +36,21 @@ class Room(models.Model):
                 return
             self.save()
     def playerNames(self, currentPlayerID):
-        return [p.user.username for p in Player.objects.filter(currentRoom=self.id) if p.id != int(currentPlayerID)]
+        return [
+            p.user.username for p in Player.objects.filter(currentRoom=self.id)
+            if p.id != int(currentPlayerID)
+        ]
     def playerUUIDs(self, currentPlayerID):
-        return [p.uuid for p in Player.objects.filter(currentRoom=self.id) if p.id != int(currentPlayerID)]
+        return [
+            p.uuid for p in Player.objects.filter(currentRoom=self.id)
+            if p.id != int(currentPlayerID)
+        ]
+
 
 class Planet(models.Model):
     title = models.CharField(max_length=50, default="DEFAULT TITLE")
     description = models.CharField(max_length=500, default="DEFAULT DESCRIPTION")
+
 
 class Player(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -64,6 +72,7 @@ class Player(models.Model):
         except PlayerVisited.DoesNotExist:
             return False
 
+
 class PlayerVisited(models.Model):
     player = models.ForeignKey(
         'Player',
@@ -73,6 +82,36 @@ class PlayerVisited(models.Model):
         'Room',
         on_delete=models.CASCADE
     )
+
+
+class MobTemplate(models.Model):
+    name = models.CharField(max_length=50, default="DEFAULT NAME")
+    description = models.CharField(max_length=500, default="DEFAULT DESCRIPTION")
+    max_health = models.IntegerField(default=50)
+    exp = models.IntegerField(default=50)
+    damage = models.IntegerField(default=50)
+    accuracy = models.IntegerField(default=50)
+    evasion = models.IntegerField(default=50)
+    idle_text = models.CharField(max_length=1000, default="DEFAULT TEXT")
+    attack_text = models.CharField(max_length=1000, default="DEFAULT TEXT")
+
+
+class Mob(models.Model):
+    template = models.ForeignKey(
+        'MobTemplate',
+        on_delete=models.CASCADE
+    )
+    health = models.IntegerField(default=50)
+    location = models.ForeignKey(
+        'MobTemplate',
+        on_delete=models.SET_NULL
+    )
+    prev_loc = models.ForeignKey(
+        'MobTemplate',
+        on_delete=models.SET_NULL
+    )
+    target = models.CharField(max_length=50, null=True)
+
 
 @receiver(post_save, sender=User)
 def create_user_player(sender, instance, created, **kwargs):
