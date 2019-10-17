2caaac593fee97d61ededc52366fdcf534f31e73
Matt Klein <mklein246@gmail.com>
2019-10-17 15:41:25 -0700

    double oops

--- adventure/urls.py
+++ adventure/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
     url('whisper', api.whisper),
     url('get_item', api.get_item),
     url('drop_item', api.drop_item),
-    url('pusher_auth', api.pusher_auth),
     url('look_item', api.look_item),
 ]
