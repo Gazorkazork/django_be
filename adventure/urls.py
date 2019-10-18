a6df4d5ef8844ee883259c02046bfeeffe578c0b
Matt Klein <mklein246@gmail.com>
2019-10-17 17:50:34 -0700

    okay trying this

--- adventure/urls.py
+++ adventure/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     url('whisper', api.whisper),
     url('get_item', api.get_item),
     url('drop_item', api.drop_item),
+    url('pusher_auth', api.pusher_auth),
     url('look_item', api.look_item),
 ]
